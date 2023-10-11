1da16ca5dd266c4541fdc71a3b6a9188214477e3
Isaac Cortes Hernandez <isaaccortes014@gmail.com>
2023-10-11 16:23:25 -0700

    added states to state select element using DOM manipulation

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,19 +15,36 @@ function addToCountrySelect() {
   );
 }
 
-function makeOptionElement(countryName) {
+function addToStateSelect() {
+  //get state select from the DOM
+  let stateSelect = document.getElementById('state');
+
+  //get data from JSON file
+  fetch('/db/states.json').then((Response) =>
+    Response.json().then((data) => {
+      // for each state
+      for (let i = 0; i < data.length; i++) {
+        //add option element to select element
+        stateSelect.appendChild(makeOptionElement(data[i].name));
+      }
+    })
+  );
+}
+
+function makeOptionElement(name) {
   //create a new element
   let myOptionEl = document.createElement('option');
   //add value attribute equal to country name
-  myOptionEl.value = countryName;
+  myOptionEl.value = name;
   //add text into the element
-  myOptionEl.text = countryName;
+  myOptionEl.text = name;
 
   return myOptionEl;
 }
 
 function main() {
   addToCountrySelect();
+  addToStateSelect();
 }
 
 main();
